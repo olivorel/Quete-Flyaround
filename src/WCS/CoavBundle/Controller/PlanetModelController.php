5c219441337186417b1d4b17b32f4a0fcdd5c779
olivorel <olivorel45@hotmail.fr>
2017-10-18 10:29:33 +0200

    CRUD flyaround

---
+++ src/WCS/CoavBundle/Controller/PlanetModelController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace WCS\CoavBundle\Controller;
+
+use WCS\CoavBundle\Entity\PlanetModel;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Planetmodel controller.
+ *
+ * @Route("planetmodel")
+ */
+class PlanetModelController extends Controller
+{
+    /**
+     * Lists all planetModel entities.
+     *
+     * @Route("/", name="planetmodel_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $planetModels = $em->getRepository('WCSCoavBundle:PlanetModel')->findAll();
+
+        return $this->render('planetmodel/index.html.twig', array(
+            'planetModels' => $planetModels,
+        ));
+    }
+
+    /**
+     * Creates a new planetModel entity.
+     *
+     * @Route("/new", name="planetmodel_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $planetModel = new Planetmodel();
+        $form = $this->createForm('WCS\CoavBundle\Form\PlanetModelType', $planetModel);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($planetModel);
+            $em->flush();
+
+            return $this->redirectToRoute('planetmodel_show', array('id' => $planetModel->getId()));
+        }
+
+        return $this->render('planetmodel/new.html.twig', array(
+            'planetModel' => $planetModel,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a planetModel entity.
+     *
+     * @Route("/{id}", name="planetmodel_show")
+     * @Method("GET")
+     */
+    public function showAction(PlanetModel $planetModel)
+    {
+        $deleteForm = $this->createDeleteForm($planetModel);
+
+        return $this->render('planetmodel/show.html.twig', array(
+            'planetModel' => $planetModel,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing planetModel entity.
+     *
+     * @Route("/{id}/edit", name="planetmodel_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, PlanetModel $planetModel)
+    {
+        $deleteForm = $this->createDeleteForm($planetModel);
+        $editForm = $this->createForm('WCS\CoavBundle\Form\PlanetModelType', $planetModel);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('planetmodel_edit', array('id' => $planetModel->getId()));
+        }
+
+        return $this->render('planetmodel/edit.html.twig', array(
+            'planetModel' => $planetModel,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a planetModel entity.
+     *
+     * @Route("/{id}", name="planetmodel_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, PlanetModel $planetModel)
+    {
+        $form = $this->createDeleteForm($planetModel);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($planetModel);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('planetmodel_index');
+    }
+
+    /**
+     * Creates a form to delete a planetModel entity.
+     *
+     * @param PlanetModel $planetModel The planetModel entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(PlanetModel $planetModel)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('planetmodel_delete', array('id' => $planetModel->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/WCS/CoavBundle/Entity/PlanetModel.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace WCS\CoavBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PlanetModel
+ *
+ * @ORM\Table(name="planet_model")
+ * @ORM\Entity(repositoryClass="WCS\CoavBundle\Repository\PlanetModelRepository")
+ */
+class PlanetModel
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="model", type="string", length=128)
+     */
+    private $model;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="manufacturer", type="string", length=64, nullable=true)
+     */
+    private $manufacturer;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="cruiseSpeed", type="smallint", nullable=true)
+     */
+    private $cruiseSpeed;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="planeNbSeats", type="smallint")
+     */
+    private $planeNbSeats;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isAvailable", type="boolean")
+     */
+    private $isAvailable;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set model
+     *
+     * @param string $model
+     *
+     * @return PlanetModel
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Get model
+     *
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set manufacturer
+     *
+     * @param string $manufacturer
+     *
+     * @return PlanetModel
+     */
+    public function setManufacturer($manufacturer)
+    {
+        $this->manufacturer = $manufacturer;
+
+        return $this;
+    }
+
+    /**
+     * Get manufacturer
+     *
+     * @return string
+     */
+    public function getManufacturer()
+    {
+        return $this->manufacturer;
+    }
+
+    /**
+     * Set cruiseSpeed
+     *
+     * @param integer $cruiseSpeed
+     *
+     * @return PlanetModel
+     */
+    public function setCruiseSpeed($cruiseSpeed)
+    {
+        $this->cruiseSpeed = $cruiseSpeed;
+
+        return $this;
+    }
+
+    /**
+     * Get cruiseSpeed
+     *
+     * @return int
+     */
+    public function getCruiseSpeed()
+    {
+        return $this->cruiseSpeed;
+    }
+
+    /**
+     * Set planeNbSeats
+     *
+     * @param integer $planeNbSeats
+     *
+     * @return PlanetModel
+     */
+    public function setPlaneNbSeats($planeNbSeats)
+    {
+        $this->planeNbSeats = $planeNbSeats;
+
+        return $this;
+    }
+
+    /**
+     * Get planeNbSeats
+     *
+     * @return int
+     */
+    public function getPlaneNbSeats()
+    {
+        return $this->planeNbSeats;
+    }
+
+    /**
+     * Set isAvailable
+     *
+     * @param boolean $isAvailable
+     *
+     * @return PlanetModel
+     */
+    public function setIsAvailable($isAvailable)
+    {
+        $this->isAvailable = $isAvailable;
+
+        return $this;
+    }
+
+    /**
+     * Get isAvailable
+     *
+     * @return bool
+     */
+    public function getIsAvailable()
+    {
+        return $this->isAvailable;
+    }
+}
+
